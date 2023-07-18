7a2fa3866c84276bb0d842d5acd40c15f7dd7d02
AbhilashGowda92 <128838354+AbhilashGowda92@users.noreply.github.com>
2023-07-18 22:31:35 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -7,6 +7,5 @@ cd /var/www/html/
 wget https://www.free-css.com/assets/files/free-css-templates/download/page278/brandon.zip
 unzip brandon.zip
 #mv solar-energy-website-template/ solar
-mv -f brandon/* .
 systemctl restart httpd
 systemctl enable httpd
